3addd2ff9e27e0a84b03efe1d509ad15b4b9e07e
Ezhil sree J <144870412+EzhilsreeJ@users.noreply.github.com>
2025-12-17 01:41:59 +0530

    Initial commit: src/pages/StudentPage.js

---
+++ src/pages/StudentPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import StudentDashboard from '../components/Dashboard/StudentDashboard';
+
+const StudentPage = () => {
+    return (
+        <div className="container">
+            <StudentDashboard />
+        </div>
+    );
+};
+
+export default StudentPage;
